073d22617414001560fb5cd30cc9e39d4ef2cd03
datngx <101488053+datnguyen2904@users.noreply.github.com>
2023-04-14 21:55:59 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Nhap a= ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap b= ");
+            b = int.Parse(Console.ReadLine());
+            int answer = 0;
+            for (int i = a; i <= b; i++)
+            {
+                answer += i;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
